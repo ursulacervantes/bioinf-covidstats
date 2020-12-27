ffc69b85ae60facbf43a48c567a410e12ec28fba
Ursula Cervantes <ursula.cervantes@n26.com>
2020-12-27 01:15:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import 'App.css'
+import Home from 'pages/Home'
+import About from 'pages/About'
+import Dashboard from 'pages/Dashboard'
+
+ const App = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
